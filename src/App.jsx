f43736a5ec907e58bb081af07871725bb2732e41
Clive Yip <cliveyip@Clives-MacBook-Pro.local>
2017-02-05 22:40:12 -0500

    styling changes and collapsible Panel

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,22 @@ import { Button } from 'react-bootstrap';
 import { ControlLabel } from 'react-bootstrap';
 import { FormControl } from 'react-bootstrap';
 import { HelpBlock } from 'react-bootstrap';
-
+import { Checkbox } from 'react-bootstrap';
+import { Panel } from 'react-bootstrap';
 
 
 class App extends Component {
+  constructor(...args){
+    super(...args);
+    this.state = {
+      open: false
+    };
+  }
   render() {
     return (
       <div className = "container">
         <div className = "row logo">
-          <img src="build/interad_logo.png" height="42"></img>
+          <img src="build/interad_logo.png" width="15%"></img>
         </div>
         <div className = "row card">
           <img src="build/Sample_CreditCard.png" width="40%" className="center-block"></img>
@@ -23,45 +30,84 @@ class App extends Component {
           </h4>
         </div>
         <div className = "row form">
-          <div className="col-md-6">
+          <div className="col-sm-6">
             <div className = "row">
-              <ControlLabel>Your Name <i>as it appears on the credit card</i></ControlLabel>
+              <ControlLabel>
+                <span> Your Name </span>
+                <span><i>as it appears on the credit card</i></span>
+              </ControlLabel>
               <FormControl type="text" placeholder="Name" />
             </div>
             <div className = "row">
-              <ControlLabel>Interad Cashback credit card number</ControlLabel>
-              <div className="col-md-3 credit-card-input">
-                <FormControl type="text" className="input-block-level" autoComplete="off" maxLength="4" pattern="\d{4}" required />
+              <div className = "row">
+                <ControlLabel>Interad Cashback credit card number</ControlLabel>
               </div>
-              <div className="col-md-3 credit-card-input">
-                <FormControl type="text" className="input-block-level" autoComplete="off" maxLength="4" pattern="\d{4}" required />
+              <div className="col-sm-3 col-md-3 col-xs-3 credit-card-input">
+                <FormControl type="text" className="input-block-level" placeholder="XXXX" autoComplete="off" maxLength="4" pattern="\d{4}" required />
               </div>
-              <div className="col-md-3 credit-card-input">
-                <FormControl type="text" className="input-block-level" autoComplete="off" maxLength="4" pattern="\d{4}" required />
+              <div className="col-sm-3 col-md-3 col-xs-3 credit-card-input">
+                <FormControl type="text" className="input-block-level" placeholder="XXXX" autoComplete="off" maxLength="4" pattern="\d{4}" required />
               </div>
-              <div className="col-md-3 credit-card-input">
-                <FormControl type="text" className="input-block-level" autoComplete="off" maxLength="4" pattern="\d{4}" required />
+              <div className="col-sm-3 col-md-3 col-xs-3 credit-card-input">
+                <FormControl type="text" className="input-block-level" placeholder="XXXX" autoComplete="off" maxLength="4" pattern="\d{4}" required />
+              </div>
+              <div className="col-sm-3 col-md-3 col-xs-3 credit-card-input">
+                <FormControl type="text" className="input-block-level" placeholder="XXXX" autoComplete="off" maxLength="4" pattern="\d{4}" required />
               </div>
             </div>
           </div>
-          <div className="col-md-6">
+          <div className="col-sm-6">
             <div className = "row">
               <ControlLabel>Email</ControlLabel>
               <FormControl type="email" placeholder="Email" />
             </div>
             <div className = "row">
-              <ControlLabel>Daytime Phone Number</ControlLabel>
-              <FormControl type="tel" placeholder="Phone Number" />
-              <HelpBlock>Help text with validation state.</HelpBlock>
+              <div className = "row">
+                <ControlLabel>Daytime Phone Number</ControlLabel>
+                <FormControl type="tel" placeholder="Phone Number" />
+                <HelpBlock>Help text with validation state.</HelpBlock>
+              </div>
             </div>
           </div>
+
           <div className = "row why-deserve-money">
             <ControlLabel>Why do you deserve this money?</ControlLabel>
             <FormControl componentClass="textarea" rows="5"  />
-
           </div>
-
           <hr />
+
+          <div className = "row business-card">
+            <Checkbox onClick= { ()=> this.setState({ open: !this.state.open })}>
+              I am registering an Interad Business Card
+            </Checkbox>
+            <Panel collapsible expanded={this.state.open}>
+              Business Name as it appears on the credit Card
+              <p>
+                I certify that I am the owner of the business
+                under which the business Interad Platinum
+                card that I've entered (into this Contest) has
+                been issued.
+              </p>
+
+            </Panel>
+          </div>
+          <hr />
+
+          <div className = "row i-have-read">
+            <Checkbox bsStyle="danger">
+              I have read and agree to the <a>Offical Contest Rules and Regulations</a>
+              <br/>
+              No purchase necessary. Contest starts Apr 1, 2013 and ends May 31, 2013.
+            </Checkbox>
+            <div className="pull-right">
+              <Button type="submit" bsStyle="danger">
+                Submit Entry
+              </Button>
+            </div>
+          </div>
+          <hr />
+
+
         </div>
 
       </div>
